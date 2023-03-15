eb9428c28b74e930ce1131157468dc9c899d58e1
RadSparkle <wwwartg1@naver.com>
2023-03-16 04:25:50 +0900

    FEAT: 스케줄러 추가

--- src/main/java/com/crawling/api/CrawlingController.java
+++ src/main/java/com/crawling/api/CrawlingController.java
@@ -2,7 +2,11 @@ package com.crawling.api;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +15,20 @@ import java.io.IOException;
 
 @RequestMapping("/crawling")
 @RestController
+@Component
 public class CrawlingController {
-    @GetMapping("/test")
-    public void test(){
-        String URL = "https://finance.naver.com/item/main.naver?code=005930";
 
+    @Scheduled(fixedDelay=10000)
+    @GetMapping("/test")
+    public void test() throws IOException {
+        String URL = "https://finance.naver.com/item/main.naver?code=005930";
         Document doc;
 
-        try {
-            doc = Jsoup.connect(URL).get();
-            Elements elem = doc.select(".new_totalinfo dl>dd");
-            System.out.println(elem);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        doc = Jsoup.connect(URL).get();
+        Elements elem = doc.select(".new_totalinfo dl>dd");
+
+        String price = elem.get(3).text().split(" ")[1];
+
+        System.out.println(price);
     }
 }
